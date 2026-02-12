b87ff84f27db7865d8eafda0afbea1886770dd78
sandeep <sbagoban@gmail.com>
2026-02-12 12:34:38 +0400

    update cors for main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Enable CORS for your frontend
   app.enableCors({
-    origin: ['http://localhost:5173'], // React dev server
+    origin: [
+      'http://localhost:5173',
+      'https://epiconcept-fe.vercel.app', // production
+    ],
     credentials: true, // needed if you use cookies or auth headers
   });
   await app.listen(process.env.PORT ?? 3000);
